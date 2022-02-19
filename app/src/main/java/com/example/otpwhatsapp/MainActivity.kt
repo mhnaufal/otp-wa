0d6e5f952fe699b27578d3dd5559f5b8a75308d4
mhnaufal <aal.pirates16@gmail.com>
2022-02-19 21:34:27 +0700

    Finish template main, login, and verification

--- app/src/main/java/com/example/otpwhatsapp/MainActivity.kt
+++ app/src/main/java/com/example/otpwhatsapp/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.example.otpwhatsapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var tombolLogin: Button
+    private lateinit var tombolVerifikasi: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        this.tombolLogin = this.findViewById(R.id.tombolLogin)
+        this.tombolLogin.setOnClickListener{
+            val intent = Intent(this, LoginActivity::class.java)
+            this.startActivity(intent)
+        }
+
+        this.tombolVerifikasi = this.findViewById(R.id.tombolVerifikasi)
+        this.tombolVerifikasi.setOnClickListener{
+            val intent = Intent(this, VerificationActivity::class.java)
+            this.startActivity(intent)
+        }
     }
 }
