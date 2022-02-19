fa0c1dd5f6264e79f5b994fee3fae249de3f5227
mhnaufal <aal.pirates16@gmail.com>
2022-02-19 21:58:05 +0700

    Update login & verification interactions

--- app/src/main/java/com/example/otpwhatsapp/LoginActivity.kt
+++ app/src/main/java/com/example/otpwhatsapp/LoginActivity.kt
@@ -1,11 +1,54 @@
 package com.example.otpwhatsapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Patterns
+import android.widget.Button
+import androidx.core.widget.doOnTextChanged
+import com.google.android.material.textfield.TextInputLayout
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var teksEditNomorTelepon: TextInputLayout
+    private lateinit var tombolKirimWA: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        this.teksEditNomorTelepon = this.findViewById(R.id.teksEditNomorTelepon)
+        this.tombolKirimWA = this.findViewById(R.id.tombolKirimWA)
+
+//        Cek format nomor telepon
+        this.teksEditNomorTelepon.editText?.doOnTextChanged { nomorTelepon, _, _, _ ->
+            if (!this.cekNomorTelepon(nomorTelepon)) {
+                this.teksEditNomorTelepon.error = "Nomor telepon salah"
+                this.tombolKirimWA.isEnabled = false
+            } else {
+                this.teksEditNomorTelepon.error = null
+                this.tombolKirimWA.isEnabled = true
+            }
+        }
+
+//        TODO: Buat fungsi kirim kode OTP di sini
+
+
+        this.tombolKirimWA.setOnClickListener {
+            val intent = Intent(this, VerificationActivity::class.java)
+            this.startActivity(intent)
+        }
+    }
+
+    private fun cekNomorTelepon(nomorTelepon: CharSequence?): Boolean {
+        if (nomorTelepon == null) {
+            return false
+        }
+        if (nomorTelepon.length < 9 || nomorTelepon.length > 20) {
+            return false
+        }
+        if (!Patterns.PHONE.matcher(nomorTelepon).matches()) {
+            return false
+        }
+        return true
     }
 }
--- app/src/main/java/com/example/otpwhatsapp/VerificationActivity.kt
+++ app/src/main/java/com/example/otpwhatsapp/VerificationActivity.kt
@@ -1,11 +1,50 @@
 package com.example.otpwhatsapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Patterns
+import android.widget.Button
+import androidx.core.widget.doOnTextChanged
+import com.google.android.material.textfield.TextInputLayout
 
 class VerificationActivity : AppCompatActivity() {
+    private lateinit var teksEditKodeOTP: TextInputLayout
+    private lateinit var tombolVerifikasi: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_verification)
+
+        this.tombolVerifikasi = this.findViewById(R.id.tombolVerifikasi)
+        this.teksEditKodeOTP = this.findViewById(R.id.teksEditKodeOTP)
+
+//        Cek format nomor telepon
+        this.teksEditKodeOTP.editText?.doOnTextChanged { kodeOTP, _, _, _ ->
+            if (!this.cekKodeOTP(kodeOTP)) {
+                this.teksEditKodeOTP.error = "Kode OTP salah format"
+                this.tombolVerifikasi.isEnabled = false
+            } else {
+                this.teksEditKodeOTP.error = null
+                this.tombolVerifikasi.isEnabled = true
+            }
+        }
+
+//        TODO: Buat fungsi verifikasi OTP di sini
+
+        this.tombolVerifikasi.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            this.startActivity(intent)
+        }
+    }
+
+    private fun cekKodeOTP(kodeOTP: CharSequence?): Boolean {
+        if (kodeOTP == null) {
+            return false
+        }
+        if (kodeOTP.length != 6) {
+            return false
+        }
+        return true
     }
 }
